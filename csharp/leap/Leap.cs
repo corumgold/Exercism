571b0e00442af46c3578443e3ed9b58448853a2a
Corum Gold <goldcorum@gmail.com>
2023-12-19 17:13:56 -0500

    Revise Leap solution

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,21 +4,6 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        if (year % 400 == 0)
-        {
-            return true;
-        }
-        else if (year % 100 == 0)
-        {
-            return false;
-        }
-        else if (year % 4 == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 }
