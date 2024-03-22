d8462111fe81555ea2e0c2f2565babdb27e51f03
Corum Gold <goldcorum@gmail.com>
2024-03-22 13:47:49 -0400

    Finish exercise 4(take 2): log levels

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -1,26 +1,15 @@
 using System;
-
 static class LogLine
 {
-    public static string Message(string logLine)
+    public static string Message(string log) => log.Substring(log.IndexOf(":") + 2).Trim();
+
+    public static string LogLevel(string log)
     {
-        int indexOfColon = logLine.IndexOf(':') + 1;
-        string message = logLine.Substring(indexOfColon, logLine.Length - indexOfColon);
-        return message.Trim();
+        int openBracketIndex = log.IndexOf("[") + 1;
+        int closeBracketIndex = log.IndexOf("]");
+        return log.Substring(openBracketIndex, closeBracketIndex - openBracketIndex).ToLower();
     }
 
-    public static string LogLevel(string logLine)
-    {
-        int indexOfOpenBracket = logLine.IndexOf("[") + 1;
-        int indexOfCloseBracket = logLine.IndexOf("]") - 1;
-        string logLevel = logLine.Substring(indexOfOpenBracket, indexOfCloseBracket);
-        return logLevel.ToLower();
-    }
-
-    public static string Reformat(string logLine)
-    {
-        string logLevel = LogLevel(logLine);
-        string message = Message(logLine);
-        return $"{message} ({logLevel})";
-    }
+    public static string Reformat(string log) => $"{Message(log)} ({LogLevel(log)})";
 }
+
